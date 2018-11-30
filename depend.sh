0c98967dddac65de7b699e4bc14d992f814f43a3
William Fouvy <fouvey@gmail.com>
2018-11-30 14:28:53 +0800

    fix depend check

--- depend.sh
+++ depend.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
 # install docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
