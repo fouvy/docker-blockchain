874ec957b5e20e7409b23d08a089c9c3ad07d87a
William Fouvy <fouvey@gmail.com>
2018-11-30 14:14:03 +0800

    fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -4,6 +4,7 @@ git pull --recurse-submodules
 for d in blockchain/* ; do
 	cd $root'/'$d
 	echo "$(pwd -P)"
+	sudo docker-compose stop
 	sudo docker-compose down --rmi all
 	sudo docker-compose up -d
 done
