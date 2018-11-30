675a8324cce854922516b70ab91ed79d428a4805
William Fouvy <fouvey@gmail.com>
2018-11-30 15:14:21 +0800

    fix curl download permission, speed up download

--- depend.sh
+++ depend.sh
@@ -14,6 +14,7 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install -y docker-ce
 # install docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /tmp/docker-compose
+sudo mv /tmp/docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
